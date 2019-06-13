48632b16c436eccf6cfd84b5086b4f201d0d679c
latif <latif.hamid@cutesafe.com>
2019-06-13 16:25:16 +0300

    displaying the list value on the layout one by one at a time

--- app/src/main/java/com/exabarermple/latif/musicalstructureapp/PlaylistActivity.java
+++ app/src/main/java/com/exabarermple/latif/musicalstructureapp/PlaylistActivity.java
@@ -1,8 +1,11 @@
 package com.exabarermple.latif.musicalstructureapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
 
@@ -20,13 +23,22 @@ public class PlaylistActivity extends AppCompatActivity {
         play_list[2] = "Playlist 3";
         play_list[3] = "Playlist 4";
         play_list[4] = "Playlist 5";
-        //displaying the play_list value as a LogCat info
-        Log.v ("For Playlist Activity","play_list[0]: is " + play_list[0]);
-        Log.v ("For Playlist Activity","play_list[1]: is " + play_list[1]);
-        Log.v ("For Playlist Activity","play_list[2]: is " + play_list[2]);
-        Log.v ("For Playlist Activity","play_list[3]: is " + play_list[3]);
-        Log.v ("For Playlist Activity","play_list[4]: is " + play_list[4]);
 
+        // displaying the value of the play_list on the layout
+
+        LinearLayout display_list_value = (LinearLayout)findViewById ( R.id.displayListViewValue );
+
+        TextView listValue1 = new TextView ( this );
+        listValue1.setText ( play_list[0] );
+        listValue1.setTextSize ( 25 );
+        listValue1.setTextColor ( Color.WHITE );
+        display_list_value.addView ( listValue1 );
+        // displaying the second value of the play_list on the layout
+        TextView listValue2 = new TextView ( this );
+        listValue2.setText ( play_list[1] );
+        listValue2.setTextSize ( 25 );
+        listValue2.setTextColor ( Color.WHITE );
+        display_list_value.addView ( listValue2 );
 
     }
 }
